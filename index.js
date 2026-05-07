0889f9691b7ea785961d988aaeaad1ee1b07c8a5
¨Ludmila¨ <ludmilapaula2206@gmail.com>
2026-05-06 23:23:04 -0300

    Agregadas funciones de Ludmila Etapa 1

---
+++ index.js
@@ -0,0 +1,6 @@
+function registrarSocio() {
+    console.log("Ejecutando: Registro de Socio (Cabecera)");
+}
+// Repetí con tus otras 3 funciones...
+
+registrarSocio(); // Llamala al final para probar
